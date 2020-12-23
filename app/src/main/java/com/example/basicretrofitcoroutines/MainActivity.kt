4a0545c2358544b137ef0ac8f23e7cbd037c71e3
Mario Gamal <mnous@justclean.com>
2020-12-23 14:13:19 +0300

    Minmize classes to get app in total of 35 LOC

--- app/src/main/java/com/example/basicretrofitcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/basicretrofitcoroutines/MainActivity.kt
@@ -6,15 +6,12 @@ import androidx.activity.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
-
     private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,13 +19,13 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class MainViewModel: ViewModel() {
-    val post = liveData { emit(MainRepository().getPost()) }
-}
+data class Post(val title: String, val body: String)
 
-class MainRepository {
-    suspend fun getPost() = ServiceProvider.getAPI().getPost()
-}
+interface ApiService { @GET("posts/1") suspend fun getPost(): Post }
+
+class MainRepository { suspend fun getPost() = ServiceProvider.getAPI().getPost() }
+
+class MainViewModel: ViewModel() { val post = liveData { emit(MainRepository().getPost()) } }
 
 object ServiceProvider {
     fun getAPI(): ApiService = Retrofit.Builder()
@@ -36,13 +33,3 @@ object ServiceProvider {
         .addConverterFactory(GsonConverterFactory.create())
         .build().create(ApiService::class.java)
 }
-
-interface ApiService {
-    @GET("posts/1") suspend fun getPost(): Post
-}
-
-data class Post(val title: String, val body: String)
-
-
-
-
